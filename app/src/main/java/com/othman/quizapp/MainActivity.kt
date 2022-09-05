37fbf633d8e1eca9ce572126c54485fa2a0c2266
Muhammad HO <othmanov777.mo@gmail.com>
2022-09-05 15:40:13 +0300

    write questions

--- app/src/main/java/com/othman/quizapp/MainActivity.kt
+++ app/src/main/java/com/othman/quizapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.othman.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editTextName : EditText = findViewById(R.id.editTextName)
+        val btnStart : Button = findViewById(R.id.btnStart)
+        btnStart.setOnClickListener(){
+
+            if (editTextName.text.isEmpty()){
+                Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this,QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+
     }
 }
